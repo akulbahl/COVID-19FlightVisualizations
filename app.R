8d8fd5be591e18da1d2fc5f784180785b65f8b45
akulbahl <65310549+akulbahl@users.noreply.github.com>
2020-10-19 23:53:28 -0400

    updated app.R

--- app.R
+++ app.R
@@ -1,7 +1,6 @@
 library(shinydashboard)
 
 
-
 dates <- cbind(c("2019-01-01", "2019-06-30"), c("2020-01-01", "2020-06-30"))
 
 
@@ -38,36 +37,57 @@ ui <- dashboardPage(
     
     dashboardBody(tabItems(
         
-        # First tab content
+  # First tab content-----------------------------------
+  
         tabItem(tabName = "intro",
                 h2("COVID-19 Flight Visualization Project")
                 ),
         
-        # Second tab content
+  # Second tab content----------------------------------
+  
         tabItem(tabName = "summary",
                 
                 fluidRow(
-                    
-                    mainPanel(plotOutput("heatdelay")),
-                    
-                    box(
-                        title = "Controls",
-                        sliderInput("slider", "Number of observations:", 1, 100, 50)
-                         )
-                    
-                        ), # END OF FIRST FLUID ROW
-                
-                radioButtons("calbuttons", label = h3("Category"),
-                             choices = list("Departure Delays > 15 min" = 1, 
-                                            "Cancelled Flights" = 2), 
-                             selected = 1),
-                hr(),
-                
-                mainPanel(plotOutput("calendar"))
-                
+                  
+                  box(title = "Flight Delay Time by Domestic US Airline (Jan 2019 - Jun 2020)",
+                    width = 6, status = "primary", mainPanel(plotOutput("heatdelay"), width = 15)
+                    ),
+                  
+                  box(title = "Elapsed Flight Time by Domestic US Airline (Jan 2019 - Jun 2020)",
+                    width = 6, status = "warning", mainPanel(plotOutput("timedensity"), width = 15)
+                  )
                 ),
+                    
+                    #box(
+                    #    title = "Controls",
+                    #    sliderInput("slider", "Number of observations:", 1, 100, 50)
+                    #     )
                 
-        # Third tab content
+                fluidRow(
+                
+                  column(12, align = "center", box(  # FIGURE OUT HOW TO CENTER
+                  width = 12, mainPanel(plotOutput("calendar"), width = 15), 
+                  
+                  box(title = "Calendar Category", width = 6, solidHeader = TRUE, 
+                      status = "primary",
+                              radioButtons("calbuttons", label = h3(""),
+                            choices = list("Total Departure Delay (in Hours)" = 1, 
+                            "Total Cancelled Flights" = 2), 
+                            selected = 1)
+                  )
+                  
+                  )
+                #box(title = "Calendar Category", width = 2, solidHeader = TRUE, status = "primary",
+                #radioButtons("calbuttons", label = h3(""),
+                #             choices = list("Total Departure Delay (in Hours)" = 1, 
+                #                            "Total Cancelled Flights" = 2), 
+                #             selected = 1)
+                    )
+                )
+        ),
+                
+  # Third tab content----------------------------------
+  
         tabItem(
             
             tabName = "widgets",
@@ -88,14 +108,22 @@ ui <- dashboardPage(
                 ),
                 
                 dateRangeInput(inputId ="dates",
-                               label = h3("Date range")),
+                               label = h3("Date range"),
+                               start = "2019-01-01",
+                                 end = "2020-06-30",
+                               min = "2019-01-01",
+                               max = "2020-06-30"),
                 hr()
                 #fluidRow(column(4, verbatimTextOutput("value")))
-            ),
+                
+                          ),
             
-            mainPanel(plotOutput("count")), #Plots delay bar chart
+            box(title = "Number of Flights",
+                mainPanel(plotOutput("barchart")) #Plots delay bar chart
+                ),
             
             #Adds buttons for boxplot
+            
             radioButtons("boxbuttons", label = h3("Category"),
                          choices = list("Pre-COVID" = 1, 
                                         "COVID" = 2), 
@@ -112,7 +140,6 @@ ui <- dashboardPage(
                 ))
 )
 
-
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
@@ -127,11 +154,34 @@ ui <- dashboardPage(
 server <- function(input, output, session) {
 
 ##--------------------------- SUMMARY PLOTS ## ---------------------------------
-  output$heatdelay <- renderPlot( 
+    
+  ## Flight Time Density ##
+    
+    output$timedensity <- renderPlot(
+        
+        ggplot(flights, aes(x = 1/60*ACTUAL_ELAPSED_TIME, fill = MKT_UNIQUE_CARRIER)) +
+        geom_density(position = 'identity',
+                     alpha = 0.8,
+                     adjust = 1) +
+        facet_grid(MKT_UNIQUE_CARRIER ~ ., scales = "free") +
+        xlim(0, 7) +
+        labs(x = 'Elapsed Flight Time (Hours)', y = 'Density') +
+        labs(fill = 'Airline') +
+        theme_minimal() +
+        theme(
+            axis.title = element_text(size = 12),
+            axis.text = element_text(size = 8),
+            legend.position = 'left'
+        )
+    )
+    
+    ## Flight Delay HEATMAP ##
+    
+    output$heatdelay <- renderPlot( 
               
                 ggplot(sum_delays, aes(FL_DATE, MKT_UNIQUE_CARRIER)) + geom_tile(aes(fill = count)) +
                 theme_minimal() +
-                labs(x = "Flight Date", y = "Airline", title = "Domestic Flight Delay Time (Jan 2019 - Jun 2020)") +
+                labs(x = "Flight Date", y = "Airline") +
                 scale_fill_gradientn(name = "Total Delay Time (Hours)",
                                      colors = c("#2A0890", "#7404AC","#E75799", "#FDAE61", "#FFFF00"),
                                      values = scales::rescale(
@@ -145,24 +195,42 @@ server <- function(input, output, session) {
     ## CALENDAR HEATMAP ##
     
     cal_heat_data <- flights %>%
-        group_by(FL_DATE) %>%
-        summarise(count = sum(DEP_DELAY_NEW, na.rm = T)) %>%
-        as.data.frame()
+      group_by(FL_DATE) %>%
+      summarise(dep_delay = 1/60 * sum(DEP_DELAY_NEW, na.rm = T),
+                cancelled = sum(CANCELLED)) %>%
+      as.data.frame()
     
-    ## Button inputs for Calendar
-    #choice = cbind(flights$DEP_DEL15, flights$CANCELLED) # 1 - DEP_DEL15, #2 - Cancelled
+            # Button and label inputs for Calendar
     
-    output$calendar <- renderPlot( 
+    calbuttonchoices = cbind(cal_heat_data$dep_delay, cal_heat_data$cancelled) # 1 - Departure Delay, #2 - Cancelled
+    calbuttontitles = c("Departure Delay Time (in Hours)", "Cancelled Flights")
+    
+            # Generates Calendar
+    
+    output$calendar <- renderPlot(
         
-        calendarHeat(cal_heat_data$FL_DATE, cal_heat_data$count #choice[, as.integer(input$calbuttons)]
-                     , ncolors = 99,
-                     color = "g2r", varname="Flight Cancellations")
+        calendarHeat(
+            cal_heat_data$FL_DATE,
+            calbuttonchoices[, as.integer(input$calbuttons)]
+            ,
+            ncolors = 99,
+            color = "g2r",
+            varname = calbuttontitles[as.integer(input$calbuttons)]
+        )
     )
     
+    ## CORRELATION HEATMAP
     
-    
+    output$corrmat <- renderPlot(
+      
+      corr_mat
+                      
+      #heatmaply_cor(cor(corr_data))
+      
+      )
     
 ##--------------------------- iNTERACTIVE PLOTS ## -----------------------------
+    
     #Selects Pre/Post-COVID Dates
     
     dates <- cbind(c("2019-01-01", "2019-06-30"), c("2020-01-01", "2020-06-30"))
@@ -170,27 +238,25 @@ server <- function(input, output, session) {
     observe({
         #Edits destination based on origin input
         dest <- unique(
-            flights %>%
+            flights %>% 
                 filter(flights$ORIGIN_CITY_NAME == input$origin) %>%
-                .$DEST_CITY_NAME
+                .$DEST_CITY_NAME 
         )
+        
         updateSelectizeInput(session,
                              "dest",
                              choices = dest,
                              selected = dest[1])
     })
     
-    # Future edit to restrict dates
-    
     flights_delay <- reactive({
+      
         flights %>%
             filter(ORIGIN_CITY_NAME == input$origin &
                        DEST_CITY_NAME == input$dest &
                   FL_DATE >= ymd(input$dates[1]) & FL_DATE <= ymd(input$dates[2])) %>%
-                       filter(!(CANCELLATION_CODE %in% 
-                                    "")) %>%
+                       filter(!(CANCELLATION_CODE %in% "")) %>%
                
-                 
             group_by(MKT_UNIQUE_CARRIER) %>%
             summarise(
                 n = n(),
@@ -200,14 +266,22 @@ server <- function(input, output, session) {
         
     })
     
-    output$count <- renderPlot(
+    ## Creates Bar Chart Output
+    
+    output$barchart <- renderPlot(
         
-        flights_delay() %>%
-            ggplot(aes(x = MKT_UNIQUE_CARRIER, y = n, fill = MKT_UNIQUE_CARRIER)) +
-            geom_col(fill = "lightblue") +
-            ggtitle("Number of flights")
+      flights_delay() %>%
+        
+        mutate(MKT_UNIQUE_CARRIER = fct_reorder(MKT_UNIQUE_CARRIER, desc(n))) %>% 
+        ggplot(aes(x = MKT_UNIQUE_CARRIER, y = n, fill = MKT_UNIQUE_CARRIER)) +
+        labs(x = 'Airline', y = 'Number of Flights') +
+        geom_bar(stat = "identity") +
+        theme(axis.title = element_text(size = 14),
+              axis.text = element_text(size = 12))
+      
     )
    
+    ## Creates Boxplot Output
     
     output$boxplot <- renderPlot(
         
@@ -220,7 +294,7 @@ server <- function(input, output, session) {
         aes(x = "", y = DEP_TIME, fill = MKT_UNIQUE_CARRIER) +
         geom_boxplot() +
         scale_fill_hue() +
-        labs(x = 'Airlines', y = "Departure Time (Military)", title = "Departure Times by Airline", 
+        labs(x = 'Airline', y = "Departure Time (Military)", title = "Departure Times by Airline", 
              subtitle =  c("Jan 2019 - June 2019", "Jan 2020 - June 2020")[as.integer(input$boxbuttons)]
              , fill = "Airline") +
         theme_minimal()
