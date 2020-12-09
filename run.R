ff06cd2fc134abdcf08e10fc101f1e3448380f8c
akulbahl <65310549+akulbahl@users.noreply.github.com>
2020-12-08 20:20:16 -0500

    Add files via upload

--- run.R
+++ run.R
@@ -1,8 +1,6 @@
 library(shiny)
 
-port <- Sys.getenv('PORT')
 shiny::runApp(
   appDir = "C:\\Users\\Akul\\Desktop\\DSA\\Projects\\R Visualization\\RShinyFlightVisualizations",
-  host = '0.0.0.0',
-  port = as.numeric(port)
+  host = '0.0.0.0'
 )
