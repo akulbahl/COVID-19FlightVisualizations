58791244072b17256e4b6bd9cfecd0c159e23f63
akulbahl <65310549+akulbahl@users.noreply.github.com>
2020-12-08 20:43:26 -0500

    Add files via upload

--- run.R
+++ run.R
@@ -1,6 +1,6 @@
 library(shiny)
 
 shiny::runApp(
-  appDir = "C:\\Users\\Akul\\Desktop\\DSA\\Projects\\R Visualization\\RShinyFlightVisualizations",
+  appDir = "C:\\Users\\Akul\\Desktop\\DSA\\Projects\\R Visualization\\RShinyFlightVisualizations\\app.R",
   host = '0.0.0.0'
 )
